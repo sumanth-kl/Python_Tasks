b7af64d8eac12465a22d41857d9344a7b9fdeeef
sumanth-kl <sumanth.nov1@gmail.com>
2026-03-27 10:30:10 +0530

    Update String_Check_Substring_5.py

--- Day_4_Tasks/String_Check_Substring_5.py
+++ Day_4_Tasks/String_Check_Substring_5.py
@@ -1,5 +1,7 @@
 # 5. Write a program to check whether a substring exists in a string.
 
 text="Hi Welcome, this is python course"
+print("String is: ",text)
+print("find python")
 if "python" in text:
     print("Substring found!")
