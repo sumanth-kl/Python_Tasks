a648f3d60a73a8529a1e2b54b6c3cbc2ec16e386
sumanth-kl <sumanth.nov1@gmail.com>
2026-03-27 12:29:04 +0530

    complete day 4 task

--- Day_4_Tasks/Functions_Sum_Of_Lists_8.py
+++ Day_4_Tasks/Functions_Sum_Of_Lists_8.py
@@ -4,11 +4,8 @@ li=[2,4,5,6,7,21]
 
 def list_total():
     summ=0
-    for i in range(li[0],li[-1]):
-        summ=summ+li[0]
-        #li[0]+=1
-        if li[-1]==True:
-            break
+    for i in range(len(li)):
+        summ=summ+li[i]
     print(summ)
 
 list_total()
